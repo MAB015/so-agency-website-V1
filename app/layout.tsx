fcf3c81c5fe67bcb5a06ee41d189cbbc6e04abe2
Oscar Carabali <112968918+oscarc73@users.noreply.github.com>
2026-08-02 15:59:57 -0500

    Installing the Meta Pixel Code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { SmoothScroll } from '@/components/smooth-scroll'
 import { CustomCursor } from '@/components/custom-cursor'
 import { StarBackground } from '@/components/star-background'
 import { RightClickCTA } from '@/components/right-click-cta'
+import Script from 'next/script'
 
 // Initialize fonts
 const _geist = Geist({ subsets: ['latin'], weight: ["100","200","300","400","500","600","700","800","900"] })
@@ -74,6 +75,24 @@ export default function RootLayout({
         <meta name="twitter:image" content={`${BASE_URL}/og-image.png`} />
       </head>
       <body className={`font-sans antialiased ${_audiowide.variable} ${_roboto.variable}`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1589857299225670');
+          fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1589857299225670&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <StarBackground />
         <CustomCursor />
         <RightClickCTA />
